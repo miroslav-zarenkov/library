6a2f4bcca59603dbbb262e1394a9fabcdc5f01d6
Miroslav Zarenkov <miroslav.zarenkov@gmail.com>
2021-10-06 18:44:22 +0300

    Trying some function

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ let book;
 let submitButton = document.querySelector("#submit-button");
 let div = document.createElement("div");
 let addBookDiv = document.querySelector(".add-book");
+let bookList = document.querySelector(".book-list");
 submitButton.addEventListener("click", addBookToLibrary);
-addFirstBook();
+addSomeBooks();
+displayBooks();
 
 function Book(title, author, pages, read) {
     this.title = title,
@@ -13,12 +15,11 @@ function Book(title, author, pages, read) {
         this.read = read
 }
 
-function addFirstBook() {
+function addSomeBooks() {
     book = new Book("The Wisdom Of Crowds", "Joe Abercrombie", "528", false);
     myLibrary.push(book);
-
-    div.innerHTML = book.title + " " + book.author + " " + book.pages + " " + book.read;
-    addBookDiv.appendChild(div);
+    book = new Book("A Game of Thrones", "George R.R. Martin", "720", true);
+    myLibrary.push(book);
 }
 
 function addBookToLibrary() {
@@ -34,10 +35,22 @@ function addBookToLibrary() {
     document.querySelector("#book-author").value = "";
     document.querySelector("#book-pages").value = "";
     document.querySelector("#book-read").checked = false;
+    bookList.innerHTML = "";
+    displayBooks();
+}
+
+function displayBooks() {
+    for (i = 0; i < myLibrary.length; i++) {
+        //let newDiv = document.createElement("div");
+        // div.innerHTML = JSON.stringify(myLibrary);
+        //div.classList.add("divclass");
+        //addBookDiv.appendChild(newDiv);
+        //     console.log(JSON.stringify(myLibrary[i]))
 
 
-    let newDiv = document.createElement("div");
-    newDiv.innerHTML = book.title + " " + book.author + " " + book.pages + " " + book.read;
-    div.appendChild(newDiv);
-
+        //let newDiv = document.createElement("div");
+        bookList.innerHTML += "<div class='what'>" + myLibrary[i].title + myLibrary[i].author + myLibrary[i].pages + myLibrary[i].read + "</div>";
+        //console.log(newDiv.innerHTML);
+        //addBookDiv.appendChild(newDiv);
+    }
 }
