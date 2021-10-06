217eec8189c5c89e13e500db5388c41c2623ef5d
Miroslav Zarenkov <miroslav.zarenkov@gmail.com>
2021-10-06 13:42:47 +0300

    Add HTML, CSS, start working on functions

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${read}`
-    }
+    this.title = title,
+        this.author = author,
+        this.pages = pages,
+        this.read = read
 }
-let aGameOfThrones = new Book("A Game of Thrones", "George R.R. Martin", "720", "read")
-let theWisdomOfCrowds = new Book("The Wisdom of Crowd", "Joe Abercrombie", "528", "not read")
-console.log(aGameOfThrones.info());
-console.log(theWisdomOfCrowds.info());
+
+function addBookToLibrary() {
+    myLibrary.push(bible, aGameOfThrones, theWisdomOfCrowds);
+}
+
+let bible = new Book("bible", "god", "1000", false);
+let aGameOfThrones = new Book("A Game of Thrones", "George R.R. Martin", "720", false);
+let theWisdomOfCrowds = new Book("The Wisdom of Crowd", "Joe Abercrombie", "528", false);
