c130b14ba4781899c1b8ac0e2879840d742525e7
Miroslav Zarenkov <miroslav.zarenkov@gmail.com>
2021-10-06 17:14:49 +0300

    Add basic functionality

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let myLibrary = [];
+let book;
+let submitButton = document.querySelector("#submit-button");
+let div = document.createElement("div");
+let addBookDiv = document.querySelector(".add-book");
+submitButton.addEventListener("click", addBookToLibrary);
+addFirstBook();
 
 function Book(title, author, pages, read) {
     this.title = title,
@@ -7,16 +13,31 @@ function Book(title, author, pages, read) {
         this.read = read
 }
 
+function addFirstBook() {
+    book = new Book("The Wisdom Of Crowds", "Joe Abercrombie", "528", false);
+    myLibrary.push(book);
+
+    div.innerHTML = book.title + " " + book.author + " " + book.pages + " " + book.read;
+    addBookDiv.appendChild(div);
+}
+
 function addBookToLibrary() {
-    let book = new Book(
+    book = new Book(
         document.querySelector("#book-title").value,
         document.querySelector("#book-author").value,
         document.querySelector("#book-pages").value,
-        document.querySelector("#book-read").checked)
+        document.querySelector("#book-read").checked
+    )
 
     myLibrary.push(book);
     document.querySelector("#book-title").value = "";
     document.querySelector("#book-author").value = "";
     document.querySelector("#book-pages").value = "";
     document.querySelector("#book-read").checked = false;
+
+
+    let newDiv = document.createElement("div");
+    newDiv.innerHTML = book.title + " " + book.author + " " + book.pages + " " + book.read;
+    div.appendChild(newDiv);
+
 }
