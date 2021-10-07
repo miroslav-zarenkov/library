fe21587ec2e0f1c48afb79bd3996f76cbf1a02f7
Miroslav Zarenkov <miroslav.zarenkov@gmail.com>
2021-10-07 14:46:21 +0300

    Add booklist grid, add-book function, work on form, small fixes

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let myLibrary = [];
 let book;
 let submitButton = document.querySelector("#submit-button");
-let div = document.createElement("div");
-let addBookDiv = document.querySelector(".add-book");
 let bookList = document.querySelector(".book-list");
+let addBookForm = document.querySelector(".add-book-form")
+let addBookButton = document.querySelector("#add-book-button")
+
 submitButton.addEventListener("click", addBookToLibrary);
+submitButton.addEventListener("click", closeForm);
+addBookButton.addEventListener("click", openForm);
+
 addSomeBooks();
 displayBooks();
+closeForm();
 
 function Book(title, author, pages, read) {
     this.title = title,
@@ -35,22 +40,49 @@ function addBookToLibrary() {
     document.querySelector("#book-author").value = "";
     document.querySelector("#book-pages").value = "";
     document.querySelector("#book-read").checked = false;
-    bookList.innerHTML = "";
+    resetBookList();
     displayBooks();
+}
+
+function resetBookList() {
+    bookList.innerHTML = "";
 }
 
 function displayBooks() {
     for (i = 0; i < myLibrary.length; i++) {
-        //let newDiv = document.createElement("div");
-        // div.innerHTML = JSON.stringify(myLibrary);
-        //div.classList.add("divclass");
-        //addBookDiv.appendChild(newDiv);
-        //     console.log(JSON.stringify(myLibrary[i]))
+        const bookCard = document.createElement("div");
+        const bookCardTitle = document.createElement("div");
+        const bookCardAuthor = document.createElement("div");
+        const bookCardPages = document.createElement("div");
+        const bookCardRead = document.createElement("div")
 
+        bookCard.classList.add("book-card");
+        bookCardTitle.classList.add("book-card-title");
+        bookCardAuthor.classList.add("book-card-author");
+        bookCardPages.classList.add("book-card-pages");
+        bookCardRead.classList.add("book-card-read");
 
-        //let newDiv = document.createElement("div");
-        bookList.innerHTML += "<div class='what'>" + myLibrary[i].title + myLibrary[i].author + myLibrary[i].pages + myLibrary[i].read + "</div>";
-        //console.log(newDiv.innerHTML);
-        //addBookDiv.appendChild(newDiv);
+        bookCardTitle.textContent = myLibrary[i].title;
+        bookCardAuthor.textContent = myLibrary[i].author;
+        bookCardPages.textContent = myLibrary[i].pages;
+        if (myLibrary[i].read) {
+            bookCardRead.textContent = "Read";
+        } else {
+            bookCardRead.textContent = "Not read";
+        }
+
+        bookCard.appendChild(bookCardTitle);
+        bookCard.appendChild(bookCardAuthor);
+        bookCard.appendChild(bookCardPages);
+        bookCard.appendChild(bookCardRead)
+        bookList.appendChild(bookCard);
     }
+}
+
+function openForm() {
+    addBookForm.style.display = "block";
+}
+
+function closeForm() {
+    addBookForm.style.display = "none";
 }
