2c8921ab5c03e03c813813e980b3da0828f0262c
Miroslav Zarenkov <miroslav.zarenkov@gmail.com>
2021-11-25 16:07:18 +0300

    Changed constructor to class

--- script.js
+++ script.js
@@ -16,11 +16,20 @@ addBookButton.addEventListener("click", openForm);
 getFromLocalStorage();
 displayBooks();
 
-function Book(title, author, pages, read) {
-    this.title = title,
-        this.author = author,
-        this.pages = pages,
-        this.read = read
+// function Book(title, author, pages, read) {
+//     this.title = title,
+//         this.author = author,
+//         this.pages = pages,
+//         this.read = read
+// }
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function addSomeBooks() {
